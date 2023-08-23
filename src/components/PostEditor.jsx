8590b52961b9adab19844d29d4d599ea1a06ca26
ElisabethFox <lisa.black.fox17@gmail.com>
2023-08-23 21:23:39 +0300

    add forms to Post Editor

--- src/components/PostEditor.jsx
+++ src/components/PostEditor.jsx
@@ -1,6 +1,22 @@
+import Form from 'react-bootstrap/Form';
+
 const PostEditor = () => {
     return (
         <div className="post-container post-editor">
+            <h1 className="post-editor__title">Settings</h1>
+            
+            <Form>
+
+            <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Title" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Some post text" />
+            </Form.Group>
+
+            </Form>
+
         </div>
     );
 }
