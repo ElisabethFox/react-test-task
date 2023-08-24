047644fa6a8d5ca3543086f4954a52228f29e22b
ElisabethFox <lisa.black.fox17@gmail.com>
2023-08-24 19:58:44 +0300

    add Toggle Btn

--- src/components/post-editor/Toggle.jsx
+++ src/components/post-editor/Toggle.jsx
@@ -1,19 +1,23 @@
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
-import ToggleButton from 'react-bootstrap/ToggleButton';
+import { Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 const Toggle = ({ title }) => {
+    const [isActive, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(!isActive);
+    };
+
     return (
-    <ToggleButtonGroup name="toggle" type="radio" variant="primary">
-
-        <ToggleButton value={false}>
-        </ToggleButton>
-
-        <ToggleButton value={true}>
-        </ToggleButton>
-
-        <p>{title}</p>
-    </ToggleButtonGroup>
-    );
+        <Form className="toggle-btn">
+          <Form.Check
+            type="switch"
+            label={title}
+            onClick={handleClick}
+            active={isActive.toString()}
+          />
+        </Form>
+      );
 }
  
 export default Toggle;
