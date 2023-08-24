962ab4340ca4f864bf60b3515ed051603ba87f0a
ElisabethFox <lisa.black.fox17@gmail.com>
2023-08-24 19:17:24 +0300

    add props to Post Content Card

--- src/components/post-preview/PostPreview.jsx
+++ src/components/post-preview/PostPreview.jsx
@@ -1,9 +1,13 @@
 import PostContentCard from "./PostContentCard";
+import usePostData from "./../../hooks/usePostData";
 
 const PostPreview = () => {
+    const { postData } = usePostData();
+    const { title, postText, header } = postData;
+
     return (
         <div className="post-container post-preview">
-            <PostContentCard />
+            <PostContentCard title={title} text={postText} header={header} />
         </div>
     );
 }
--- src/hooks/usePostData.js
+++ src/hooks/usePostData.js
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
 import { PostDataContext } from '../context/PostDataContext';
 
-export const usePostData = () => useContext(PostDataContext);
+const usePostData = () => useContext(PostDataContext);
+
+export default usePostData;
